aa86e572c19e21d4c0a460e15c7e3f57f5395ec5
stepan.volski <stepan.volski@gmail.com>
2023-10-04 17:08:57 +0200

    add author selector to messages; add avatars

--- iq-messenger-client/src/app/app.module.ts
+++ iq-messenger-client/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatIconModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- iq-messenger-client/src/app/components/chat-footer/chat-footer.component.ts
+++ iq-messenger-client/src/app/components/chat-footer/chat-footer.component.ts
@@ -8,18 +8,23 @@ import { WebsocketService } from 'src/app/services/websocket.service';
   styleUrls: ['./chat-footer.component.scss'],
 })
 export class ChatFooterComponent {
-  value = '';
+  message = '';
+  author = '';
 
   constructor(private websocketService: WebsocketService) {}
 
   sendMsg() {
     const message: Message = {
-      author: 'Aleksey',
-      content: this.value,
+      author: this.author,
+      content: this.message,
       timeStamp: new Date(),
     };
 
     this.websocketService.messages.next(message);
-    this.value = '';
+    this.message = '';
+  }
+
+  onAuthorToggleChange(event: any) {
+    this.author = event.value;
   }
 }

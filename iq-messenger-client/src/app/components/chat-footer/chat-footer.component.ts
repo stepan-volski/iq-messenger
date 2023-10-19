aa33ebaffa797b63b0440d25013187227765ffcd
stepan.volski <stepan.volski@gmail.com>
2023-10-19 17:22:59 +0200

    improve message input field

--- iq-messenger-client/src/app/components/chat-footer/chat-footer.component.ts
+++ iq-messenger-client/src/app/components/chat-footer/chat-footer.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Message } from 'src/app/models/Message';
 import { WebsocketService } from 'src/app/services/websocket.service';
-import { Store } from '@ngrx/store';
-import { selectUserState } from 'src/app/store/user-store/selectors';
-import { UserStoreActions } from 'src/app/store/user-store/actions';
 import { RootStoreState } from 'src/app/store';
+import { selectUserState } from 'src/app/store/user-store/selectors';
 
 @Component({
   selector: 'app-chat-footer',
   templateUrl: './chat-footer.component.html',
   styleUrls: ['./chat-footer.component.scss'],
 })
-export class ChatFooterComponent {
+export class ChatFooterComponent implements OnInit {
   message = '';
   author!: string | null;
+  @ViewChild('input') inputField!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  focusInputField() {
+    if (this.inputField) this.inputField.nativeElement.focus();
+  }
 
   constructor(
     private store$: Store<RootStoreState.State>,
@@ -30,13 +41,14 @@ export class ChatFooterComponent {
   }
 
   sendMsg() {
-    const message: Message = {
-      author: this.author,
-      content: this.message,
-      timeStamp: new Date(),
-    };
-
-    this.websocketService.messages.next(message);
-    this.message = '';
+    if (this.message.length > 0) {
+      const message: Message = {
+        author: this.author,
+        content: this.message,
+        timeStamp: new Date(),
+      };
+      this.websocketService.messages.next(message);
+      this.message = '';
+    }
   }
 }
